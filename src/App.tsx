5c3bab05d27a0d183a62fec42c134951fe739a51
CBBell99 <chrisbeckertonbell@gmail.com>
2023-02-24 11:32:22 -0800

    pull data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import "./App.scss";
+import { seatingTables } from "./assets/data/dummyData";
+
 function App() {
+  console.log(seatingTables);
+
   return (
     <main>
       <h1>Rocky POS</h1>
-      <section></section>
+
+      <section>
+        <h2>Select table</h2>
+        {seatingTables.map(table => (
+          <div className="table" key={table.table_id}>
+            {table.table_id}
+          </div>
+        ))}
+      </section>
     </main>
   );
 }
