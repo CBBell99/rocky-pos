e7ba9fffdf092991ef4c5c93a4d35c5bd48b33cf
CBBell99 <chrisbeckertonbell@gmail.com>
2023-02-24 07:55:30 -0800

    create dummy data for app

---
+++ src/assets/data/dummyData.ts
@@ -0,0 +1,143 @@
+const seatingTables = [
+  {
+    table_id: 1,
+    available_seats: 4,
+  },
+  {
+    table_id: 2,
+    available_seats: 4,
+  },
+  {
+    table_id: 3,
+    available_seats: 4,
+  },
+  {
+    table_id: 4,
+    available_seats: 4,
+  },
+  {
+    table_id: 5,
+    available_seats: 4,
+  },
+  {
+    table_id: 6,
+    available_seats: 4,
+  },
+  {
+    table_id: 7,
+    available_seats: 4,
+  },
+  {
+    table_id: 8,
+    available_seats: 4,
+  },
+  {
+    table_id: 9,
+    available_seats: 4,
+  },
+];
+
+const menuItems = [
+  {
+    menu_id: 1,
+    name: "pork chop",
+    category: "lunch",
+  },
+  {
+    menu_id: 2,
+    name: "power bowl",
+    category: "lunch",
+  },
+  {
+    menu_id: 3,
+    name: "short rib",
+    category: "lunch",
+  },
+  {
+    menu_id: 4,
+    name: "steak",
+    category: "lunch",
+  },
+  {
+    menu_id: 5,
+    name: "salmon",
+    category: "lunch",
+  },
+  {
+    menu_id: 6,
+    name: "chicken",
+    category: "lunch",
+  },
+  {
+    menu_id: 7,
+    name: "risotto",
+    category: "lunch",
+  },
+  {
+    menu_id: 8,
+    name: "udon bowl",
+    category: "lunch",
+  },
+  {
+    menu_id: 9,
+    name: "udon bowl",
+    category: "lunch",
+  },
+  {
+    menu_id: 10,
+    name: "souffle",
+    category: "breakfast",
+  },
+  {
+    menu_id: 11,
+    name: "eggs benedict",
+    category: "breakfast",
+  },
+  {
+    menu_id: 12,
+    name: "skillet",
+    category: "breakfast",
+  },
+  {
+    menu_id: 13,
+    name: "pancakes",
+    category: "breakfast",
+  },
+  {
+    menu_id: 14,
+    name: "scramble",
+    category: "breakfast",
+  },
+  {
+    menu_id: 15,
+    name: "parfait",
+    category: "breakfast",
+  },
+  {
+    menu_id: 16,
+    name: "avocado toast",
+    category: "breakfast",
+  },
+  {
+    menu_id: 17,
+    name: "oatmeal",
+    category: "breakfast",
+  },
+  {
+    menu_id: 18,
+    name: "souffle",
+    category: "breakfast",
+  },
+  {
+    menu_id: 19,
+    name: "burger",
+    category: "lunch",
+  },
+  {
+    menu_id: 20,
+    name: "pasta",
+    category: "lunch",
+  },
+];
+
+export { seatingTables, menuItems };
