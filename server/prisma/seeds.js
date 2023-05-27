73d2f597b3f05db9f344b7aee948e2e7a4f3a148
CBBell99 <chrisbeckertonbell@gmail.com>
2023-05-26 22:55:37 -0700

    use es6 imports

---
+++ server/prisma/seeds.js
@@ -0,0 +1,77 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+const seed = async () => {
+  try {
+    // Delete all existing data
+    await prisma.order_items.deleteMany();
+    await prisma.orders.deleteMany();
+    await prisma.menu_items.deleteMany();
+    await prisma.employees.deleteMany();
+    await prisma.tables.deleteMany();
+
+    const employees = await prisma.employees.createMany({
+      data: [
+        { firstName: 'John', lastName: 'Doe', role: 'HOST' },
+        { firstName: 'Jane', lastName: 'Smith', role: 'KITCHEN' },
+        { firstName: 'Admin', lastName: 'User', role: 'ADMIN' },
+      ],
+    });
+
+    const menuItems = await prisma.menu_items.createMany({
+      data: [
+        {
+          itemName: 'Pancakes',
+          description: 'Fluffy pancakes with syrup',
+          price: 8,
+          category: 'BREAKFAST',
+        },
+        {
+          itemName: 'Burger',
+          description: 'Juicy beef burger with fries',
+          price: 12,
+          category: 'LUNCH',
+        },
+        {
+          itemName: 'Cheesecake',
+          description: 'Creamy cheesecake with strawberry topping',
+          price: 6,
+          category: 'DESSERT',
+        },
+      ],
+    });
+
+    const tables = await prisma.tables.createMany({
+      data: [{ available: true }, { available: false }, { available: true }],
+    });
+
+    const orders = await prisma.orders.createMany({
+      data: [
+        { employeeId: 1, tableId: 1, guestCount: 2 },
+        { employeeId: 1, tableId: 3, guestCount: 4 },
+      ],
+    });
+
+    const orderItems = await prisma.order_items.createMany({
+      data: [
+        { orderId: 1, menuItemId: 1 },
+        { orderId: 1, menuItemId: 2 },
+        { orderId: 2, menuItemId: 2 },
+        { orderId: 2, menuItemId: 3 },
+      ],
+    });
+    console.log('Seed data inserted successfully.');
+  } catch (error) {
+    console.error('Error inserting seed data:', error);
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+seed()
+  .catch(e => {
+    console.error(e.message, e);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
-// require('dotenv').config();
-const { PrismaClient } = require('@prisma/client');
-
-const PORT = process.env.PORT || 5005;
-const express = require('express');
+import { PrismaClient } from '@prisma/client';
+import express from 'express';
+const PORT = process.env.PORT || 8000;
 const prisma = new PrismaClient();
-// const dbParams = require('./lib/db');
 
 const app = express();
 
 app.get('/menus', async (req, res) => {
   const menus = await prisma.menu_categories.findMany();
   res.json(menus);
+});
+
+app.get('/employees', async (req, res) => {
+  const employees = await prisma.employees.findMany();
+  res.json(employees);
 });
 
 app.listen(PORT, () => {
