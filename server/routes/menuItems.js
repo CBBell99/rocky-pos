99e05fc22f29dbb80cfd49fb0b407402807d78db
CBBell99 <chrisbeckertonbell@gmail.com>
2023-05-29 01:58:43 -0700

    create menuRoutes file

---
+++ server/routes/menuItems.js
@@ -0,0 +1,82 @@
+const { PrismaClient } = require('@prisma/client');
+const { log } = require('console');
+const prisma = new PrismaClient();
+const express = require('express');
+const router = express.Router();
+
+router.use(express.json());
+
+// GET all menu items
+router.get('/', async (req, res) => {
+  try {
+    const menuItems = await prisma.menu_items.findMany();
+    res.status(200).json(menuItems);
+  } catch (error) {
+    console.error('Error retrieving menus:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// POST new menu item
+router.post('/', async (req, res) => {
+  try {
+    const { itemName, description, price, category } = req.body;
+    const menuItem = await prisma.menu_items.create({
+      data: {
+        itemName,
+        description,
+        price,
+        category,
+      },
+    });
+    res.status(201).json(menuItem);
+  } catch (error) {
+    console.error('Error creating menu item:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const menuItemId = +req.params.id;
+    const menuItem = await prisma.menu_items.findUnique({
+      where: { id: menuItemId },
+    });
+
+    if (!menuItem) {
+      return res.status(404).json({ error: 'Menu item not found' });
+    }
+    res.status(200).json(menuItem);
+  } catch (error) {
+    console.error('Error retrieving menu item', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+router.patch('/:id', async (req, res) => { 
+  try {
+    const menuItemId = +req.params.id
+    const updatedFields = req.body
+
+
+    const menuItem = await prisma.menu_items.findUnique({
+      where: { id: menuItemId }
+    })
+
+    if (!menuItem) {
+      return res.status(404).json({ error:"Menu item not found"})
+    }
+
+    const updatedMenuItem = await prisma.menu_items.update({
+      where: { id: menuItemId },
+      data: updatedFields
+    })
+    res.status(200).json(updatedMenuItem)
+  } catch (error) {
+    console.error('Error updating menu item', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+
+})
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,18 +1,25 @@
-import { PrismaClient } from '@prisma/client';
-import express from 'express';
-const PORT = process.env.PORT || 8000;
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+const express = require('express');
+const morgan = require('morgan');
+
+const PORT = process.env.PORT || 8000;
 
 const app = express();
+app.use(morgan('dev'));
 
-app.get('/menus', async (req, res) => {
-  const menus = await prisma.menu_categories.findMany();
-  res.json(menus);
-});
+const menuItemRoute = require('../server/routes/menuItems');
+
+app.use('/menu', menuItemRoute);
 
 app.get('/employees', async (req, res) => {
-  const employees = await prisma.employees.findMany();
-  res.json(employees);
+  try {
+    const employees = await prisma.employees.findMany();
+    res.json(employees);
+  } catch (error) {
+    console.error('Error retrieving employess', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
 app.listen(PORT, () => {
